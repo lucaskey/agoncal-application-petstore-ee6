fb4118c3b18c7f49a53f67398c9703e8b6b6fbbe
Lucas Key Shibata Mitsudo <lucasmitsudo@gmail.com>
2022-05-03 09:36:11 -0300

    @getter @setter do Address

--- src/main/java/org/agoncal/application/petstore/domain/Address.java
+++ src/main/java/org/agoncal/application/petstore/domain/Address.java
@@ -5,6 +5,9 @@ import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author Antonio Goncalves
  *         http://www.antoniogoncalves.org
@@ -44,6 +47,8 @@ public class Address {
     public Address() {
     }
 
+    @Getter 
+    @Setter
     public Address(String street1, String city, String zipcode, String country) {
         this.street1 = street1;
         this.city = city;
@@ -55,54 +60,7 @@ public class Address {
     // =         Getters & setters          =
     // ======================================
 
-    public String getStreet1() {
-        return street1;
-    }
-
-    public void setStreet1(String street1) {
-        this.street1 = street1;
-    }
-
-    public String getStreet2() {
-        return street2;
-    }
-
-    public void setStreet2(String street2) {
-        this.street2 = street2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
+  
     // ======================================
     // =   Methods hash, equals, toString   =
     // ======================================
