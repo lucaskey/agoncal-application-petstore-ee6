d450abd670a92174080b1f52142e835d9c7a5f44
Lucas Key Shibata Mitsudo <lucasmitsudo@gmail.com>
2022-05-03 09:42:02 -0300

    @getter @setter da classe CartItem

--- src/main/java/org/agoncal/application/petstore/domain/CartItem.java
+++ src/main/java/org/agoncal/application/petstore/domain/CartItem.java
@@ -3,12 +3,17 @@ package org.agoncal.application.petstore.domain;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author Antonio Goncalves
  *         http://www.antoniogoncalves.org
  *         --
  */
 
+@Getter
+@Setter
 public class CartItem {
 
     // ======================================
@@ -42,21 +47,6 @@ public class CartItem {
     // =         Getters & setters          =
     // ======================================
 
-    public Item getItem() {
-        return item;
-    }
-
-    public void setItem(Item item) {
-        this.item = item;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
 
     // ======================================
     // =   Methods hash, equals, toString   =
